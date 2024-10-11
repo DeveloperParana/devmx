f2d47ea09e13205e6dd70759726c5fc494e67125
Gui Seek <guiseek@gmail.com>
2024-10-11 01:55:54 -0300

    feat: adiciona suporte a mais 118 ícones svg

---
+++ packages/shared/ui-global/icon/src/lib/icon-registry.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { of, tap } from 'rxjs';
+import { Icon } from './types';
+
+export class IconRegistry {
+  #cache: Partial<Record<Icon, string>> = {};
+
+  constructor(private http: HttpClient) {}
+
+  getIcon(name: Icon) {
+    if (this.#cache[name]) return of(this.#cache[name]);
+
+    return this.http
+      .get(`icons/${name}.svg`, { responseType: 'text' })
+      .pipe(tap(this.#setCache(name)));
+  }
+
+  #setCache = (name: Icon) => (icon: string) => {
+    this.#cache[name] = icon;
+  };
+}
---
+++ packages/shared/ui-global/icon/src/lib/types/emoji.ts
@@ -0,0 +1,22 @@
+export type Emoji =
+  | 'angry'
+  | 'big-smile'
+  | 'confused'
+  | 'crying'
+  | 'evil'
+  | 'happy'
+  | 'neutral'
+  | 'sad-alt'
+  | 'sad'
+  | 'shoked'
+  | 'smirking-alt'
+  | 'smirking'
+  | 'stressed'
+  | 'suspicious'
+  | 'tongue-out'
+  | 'very-happy'
+  | 'whistling'
+  | 'wink'
+  | 'wowed';
+
+export type EmojiIcon = `emoji/${Emoji}`;
