664853b5d052d8a02a11c3f55f936d6528d88184
Gui Seek <guiseek@gmail.com>
2024-10-30 01:23:48 -0300

    feat(presentation): presentation filter chips

--- packages/presentation/feature-shell/src/lib/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './presentation-comment/presentation-comment.component';
-export * from './presentation-filter/presentation-filter.component';
-export * from './presentation-card/presentation-card.component';
-export * from './reaction/reaction.component';
--- packages/presentation/ui-shared/src/lib/components/presentation-filter/presentation-filter.component.ts
+++ packages/presentation/ui-shared/src/lib/components/presentation-filter/presentation-filter.component.ts
@@ -1,55 +1,22 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 import { PresentationFormat } from '@devmx/shared-api-interfaces';
-import { PresentationFormatPipe } from '../../pipes';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import {
-  radio,
-  TypedFields,
-  FormGroupComponent,
-  createFormGroup,
-} from '@devmx/shared-ui-global/forms';
-
-interface PresentationFilter {
-  format: PresentationFormat;
-}
+import { FormOption } from '@devmx/shared-ui-global/forms';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'devmx-presentation-filter',
-  template: `<devmx-form-group
-    [fields]="fields"
-    [formGroup]="formGroup"
-    (valueChanges)="onFilterChange($event)"
-  />`,
+  templateUrl: './presentation-filter.component.html',
   styleUrl: './presentation-filter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    RouterModule,
-    ReactiveFormsModule,
-    FormGroupComponent,
-    PresentationFormatPipe,
-  ],
+  imports: [MatChipsModule],
   standalone: true,
 })
 export class PresentationFilterComponent {
-  router = inject(Router);
+  change = output<PresentationFormat>();
 
-  fields: TypedFields<PresentationFilter> = {
-    format: radio({
-      label: 'Formato',
-      value: '',
-      options: [
-        { value: '', text: 'Todos' },
-        { value: 'talk', text: 'Palestra' },
-        { value: 'workshop', text: 'Oficina' },
-        { value: 'webinar', text: 'Seminário' },
-      ],
-    }),
-  };
-
-  formGroup = createFormGroup(this.fields);
-
-  onFilterChange(queryParams: PresentationFilter) {
-    this.router.navigate([], { queryParams });
-  }
+  formats: FormOption<PresentationFormat>[] = [
+    { value: 'talk', viewValue: 'Palestra' },
+    { value: 'workshop', viewValue: 'Oficina' },
+    { value: 'webinar', viewValue: 'Seminário' },
+  ];
 }
