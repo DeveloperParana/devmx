a1287fff391b5a6341aee74d80de7094ae573ea7
Gui Seek <guiseek@gmail.com>
2024-11-12 18:42:30 -0300

    chore: dx

--- tools/plugin/dx-dev/src/generators/entity/lib/normalize-options.ts
+++ tools/plugin/dx-dev/src/generators/entity/lib/normalize-options.ts
@@ -1,11 +1,25 @@
+import { pluralize } from '../../../utils';
 import { names } from '@nx/devkit';
 import {
   EntityGeneratorSchema,
   NormalizedEntityGeneratorSchema,
 } from '../schema';
 
+function getAllNames(original: string) {
+  const normalized = names(original);
+  const pluralNames = {
+    namePlural: pluralize(normalized.name, 2),
+    classNamePlural: pluralize(normalized.className, 2),
+    fileNamePlural: pluralize(normalized.fileName, 2),
+    propertyNamePlural: pluralize(normalized.propertyName, 2),
+    constantNamePlural: pluralize(normalized.constantName, 2),
+  };
+  return { ...normalized, ...pluralNames };
+}
+
 export function normalizeOptions(
   options: EntityGeneratorSchema
 ): NormalizedEntityGeneratorSchema {
-  return { ...options, ...names(options.name) };
+  const allNames = getAllNames(options.name);
+  return { ...options, ...allNames };
 }
