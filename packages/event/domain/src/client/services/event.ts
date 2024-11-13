399d4ac7808d95e343b9074f7880fa5fb6744dee
Gui Seek <guiseek@gmail.com>
2024-11-13 16:46:37 -0300

    feat(event): mostra apenas eventos futuros

--- packages/event/domain/src/client/services/event.ts
+++ packages/event/domain/src/client/services/event.ts
@@ -1,8 +1,10 @@
-import { Event, RSVP, RSVPStatus } from '@devmx/shared-api-interfaces';
+import { Event, Page, QueryParams, RSVP, RSVPStatus } from '@devmx/shared-api-interfaces';
 import { EntityService } from '@devmx/shared-api-interfaces/client';
 import { Observable } from 'rxjs';
 
 export abstract class EventService extends EntityService<Event> {
+  abstract findAll(params: QueryParams<Event>): Observable<Page<Event>>;
+
   abstract createRSVP(event: string, status: RSVPStatus): Observable<RSVP>;
 
   abstract findRSVPByEvent(event: string): Observable<RSVP[]>;
---
+++ packages/event/domain/src/server/use-cases/find-events-from.ts
@@ -0,0 +1,43 @@
+import { EventsService } from '../services';
+import {
+  Event,
+  Page,
+  UseCase,
+  QueryParams,
+} from '@devmx/shared-api-interfaces';
+
+export class FindEventsFromUseCase
+  implements UseCase<[Date,QueryParams<Event>], Page<Event>>
+{
+  constructor(private eventsService: EventsService) {}
+
+  async execute([date, params]: [Date,QueryParams<Event>]) {
+    if (params.filter) {
+      if (params.filter.format) {
+        params.filter.format = new RegExp(params.filter.format, 'i');
+      } else {
+        delete params.filter.format;
+      }
+
+      if (params.filter.title) {
+        params.filter.title = new RegExp(params.filter.title, 'i');
+      } else {
+        delete params.filter.title;
+      }
+
+      if (params.filter.city) {
+        params.filter.city = params.filter.city.toString();
+      } else {
+        delete params.filter.city;
+      }
+
+      if (params.filter.description) {
+        params.filter.description = new RegExp(params.filter.description, 'i');
+      } else {
+        delete params.filter.description;
+      }
+    }
+
+    return await this.eventsService.findFrom(date, params);
+  }
+}
