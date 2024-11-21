318e275512d9cecdf41b7d52c97b404a2f82e624
Gui Seek <guiseek@gmail.com>
2024-11-21 16:29:53 -0300

    feat(shared-ui-global): adiciona componente date-time-picker

---
+++ packages/shared/ui-global/datetime/src/lib/date-time-picker/date-time-picker.component.ts
@@ -0,0 +1,91 @@
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { DateAdapter, provideNativeDateAdapter } from '@angular/material/core';
+import { DateTimePickerForm } from './date-time-picker.form';
+import { MatInputModule } from '@angular/material/input';
+import {
+  Self,
+  inject,
+  Optional,
+  Component,
+  Renderer2,
+  ElementRef,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import {
+  NgControl,
+  FormControl,
+  ReactiveFormsModule,
+  DefaultValueAccessor,
+} from '@angular/forms';
+
+@Component({
+  selector: 'devmx-date-time-picker',
+  templateUrl: './date-time-picker.component.html',
+  styleUrl: './date-time-picker.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatTimepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  standalone: true,
+})
+export class DateTimePickerComponent
+  extends DefaultValueAccessor
+  implements AfterViewInit
+{
+  get control() {
+    return this.ngControl.control as FormControl<Date>;
+  }
+
+  readonly _adapter = inject<DateAdapter<unknown, unknown>>(DateAdapter);
+
+  form = new DateTimePickerForm();
+
+  constructor(
+    renderer: Renderer2,
+    readonly elRef: ElementRef,
+    @Optional() @Self() public ngControl: NgControl
+  ) {
+    super(renderer, elRef, true);
+    this.ngControl.valueAccessor = this;
+
+    this._adapter.setLocale('pt-BR');
+  }
+
+  ngAfterViewInit() {
+    this.form.setValue(
+      {
+        date: this.control.value,
+        time: this.control.value,
+      },
+      { emitEvent: false }
+    );
+
+    const { date, time } = this.form.controls;
+
+    date.valueChanges.subscribe((d) => {
+      const { value } = this.control;
+      value.setDate(d.getDate());
+      value.setMonth(d.getMonth());
+      value.setFullYear(d.getFullYear());
+
+      this.control.setValue(value);
+    });
+
+    time.valueChanges.subscribe((t) => {
+      const { value } = this.control;
+      value.setHours(t.getHours());
+      value.setMinutes(t.getMinutes());
+      value.setSeconds(t.getSeconds());
+
+      this.control.setValue(value);
+    });
+  }
+}
