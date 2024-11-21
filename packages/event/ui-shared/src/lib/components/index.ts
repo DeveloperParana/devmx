1f9f4cf68603619e58563996cf55387dd0608457
Gui Seek <guiseek@gmail.com>
2024-11-21 01:35:28 -0300

    feat: adiciona botão de integração ao google calendar

--- packages/event/ui-shared/src/lib/components/index.ts
+++ packages/event/ui-shared/src/lib/components/index.ts
@@ -1,3 +1,4 @@
+export * from './calendar-button/calendar-button.component';
 export * from './event-filter/event-filter.component';
 export * from './rsvp-button/rsvp-button.component';
 export * from './event-card/event-card.component';
