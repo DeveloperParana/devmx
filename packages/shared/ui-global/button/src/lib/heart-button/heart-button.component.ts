f8c8979bbc37dad555c10f8be638046e4864dda0
Gui Seek <guiseek@gmail.com>
2024-11-21 08:46:10 -0300

    feat(shared-ui-global): adiciona entry-point button pra calendar, maps e youtube buttons

---
+++ packages/shared/ui-global/button/src/lib/heart-button/heart-button.component.ts
@@ -0,0 +1,38 @@
+import { NgClass } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { IconComponent } from '@devmx/shared-ui-global/icon';
+
+@Component({
+  selector: 'devmx-heart-button',
+  template: `
+    <button
+      mat-icon-button
+      (click)="onClick()"
+      [disabled]="disabled()"
+      [ngClass]="{ active: loved() }"
+    >
+      <devmx-icon name="heart" />
+    </button>
+  `,
+  imports: [MatIconButton, IconComponent, NgClass],
+})
+export class HeartButtonComponent {
+  loved = input(false);
+
+  onLove = output<boolean>();
+
+  disabled = input<boolean | ''>(false);
+
+  isDisabled() {
+    if (this.disabled() === '') {
+      return true;
+    }
+
+    return this.disabled();
+  }
+
+  onClick() {
+    this.onLove.emit(!this.loved());
+  }
+}
---
+++ packages/shared/ui-global/button/src/lib/youtube-button/youtube-button.component.ts
@@ -0,0 +1,44 @@
+import { Platform, PlatformModule } from '@angular/cdk/platform';
+import { IconComponent } from '@devmx/shared-ui-global/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EventPage } from '@devmx/shared-api-interfaces';
+import {
+  input,
+  inject,
+  signal,
+  Component,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector: 'devmx-youtube-button',
+  templateUrl: './youtube-button.component.html',
+  styleUrl: './youtube-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatButtonModule, PlatformModule, IconComponent],
+})
+export class YoutubeButtonComponent implements AfterViewInit {
+  data = input.required<EventPage>();
+
+  platform = inject(Platform);
+
+  link = signal('');
+
+  ngAfterViewInit() {
+    const { link } = this.data();
+
+    if (!link) return;
+
+    if (this.platform.ANDROID || this.platform.IOS) {
+      if (link.indexOf('http') >= 0) {
+        link.replace('http://', 'youtube://');
+      }
+      if (link.indexOf('https') >= 0) {
+        link.replace('https://', 'youtube://');
+      }
+    }
+
+    this.link.set(link);
+  }
+}
