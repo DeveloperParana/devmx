780834aaffd2820c52d47cc6155d30c4c9db50ef
Gui Seek <guiseek@gmail.com>
2024-11-23 23:31:39 -0300

    refactor(account): remove api interfaces desnecessária

--- packages/account/api-interfaces/src/client/services/search-leaders.ts
+++
@@ -1,6 +0,0 @@
-import { AccountOut, Page } from '@devmx/shared-api-interfaces';
-import { Observable } from 'rxjs';
-
-export abstract class SearchLeadersService {
-  abstract search(params: URLSearchParams): Observable<Page<AccountOut>>;
-}
--- packages/account/data-access/src/lib/services/index.ts
+++ packages/account/data-access/src/lib/services/index.ts
@@ -1,2 +1,1 @@
 export * from './city.impl';
-export * from './search-leaders.impl';
